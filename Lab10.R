1496aebb94c13df671f1957da79b583da3d596df
HSun8 <24sunh@gmail.com>
2025-04-05 18:49:07 -0400

    task 3 done

--- Lab10.R
+++ Lab10.R
@@ -3,27 +3,40 @@
 #
 library(tidyverse)
 library(patchwork)
+library(e1071)
 ################################################################################
 # Basic Simulation
+# parameters
 sample.size <- 1004
-polls <- 10000
-basic.sim <- rbinom(n=polls, size = sample.size, prob = 0.39)/sample.size
-# need to divide by sample size (turn into percent)
-ggplot()+
+num.polls <- 10000
+
+# want to get simulation as a percent of how many Americans are satisified
+# with US (divide by sample size)
+basic.sim <- rbinom(n=num.polls, size = sample.size, prob = 0.39)/sample.size
+
+# plot data
+first.sim <- ggplot()+
   geom_histogram(aes(x=basic.sim, y = after_stat(density)))+
   geom_density(aes(x=basic.sim))+
-  geom_hline()
+  geom_hline(yintercept = 0)+
+  theme_bw()
 
 # histogram + density is approximately normal
+# basic.sim.data <- summarize(tibble(x=basic.sim), 
+#                             mean = mean(x),
+#                             variance = var(x),
+#                             skewness = skewness(x),
+#                             e.kurtosis = kurtosis(x))
+
 middle.95 <- quantile(x=basic.sim, 0.975) - quantile(x=basic.sim, 0.025)
 moe <- 0.5*middle.95
 # moe is ~3%, compared to Gallup's 4%
 
 # again
 sample.size.new <- sample.size * 2
-double.sim <- rbinom(n=polls, size = sample.size.new, prob = 0.39)/sample.size.new
+double.sim <- rbinom(n=num.polls, size = sample.size.new, prob = 0.39)/sample.size.new
 
-ggplot()+
+second.sim <- ggplot()+
   geom_histogram(aes(x=double.sim, y = after_stat(density)))+
   geom_density(aes(x=double.sim))+
   geom_hline(yintercept=0)+
@@ -33,14 +46,24 @@ d.middle.95 <- quantile(x=double.sim, 0.975) - quantile(x=double.sim, 0.025)
 d.moe <- 0.5*d.middle.95
 # moe is ~ 2.17%, compared to Gallup's 2% 
 
+# histogram + density still normal, however, variance is smaller
+# moe and middle 95 also smaller for larger sample esize
+
+task1.sims <- first.sim + second.sim
 ################################################################################
-# resampling
+# Resampling
+
 # create a tibble with gallup data: .39 satisfied, .59 unsatisified, .02 no answer
+# round to catch all samples
 satisfied <- rep(1, times = round(0.39 * sample.size))
 unsatisfied <- rep(0, times = round(0.59 * sample.size))
 no.opinion <- rep(NA, times = round(0.02 * sample.size))
+
+
+# combine into tibble
 gallup.data <- tibble(gallup = c(satisfied, unsatisfied, no.opinion))
 
+# resampling
 resamples <- 1000
 resamples.data <- tibble(p.hat = numeric(resamples))
 for (i in 1:resamples){
@@ -51,7 +74,8 @@ for (i in 1:resamples){
   resamples.data$p.hat[i] <- mean(curr.resample, na.rm = T)
   
 }
-ggplot(data = resamples.data)+
+
+resampling.plot <- ggplot(data = resamples.data)+
   geom_histogram(aes(x = p.hat, y = after_stat(density)))+
   geom_density(aes(x=p.hat))
 
@@ -59,3 +83,30 @@ resample.middle95 <- quantile(x=resamples.data$p.hat, 0.975) -
                      quantile(x=resamples.data$p.hat, 0.025)
 resample.moe <- 0.5 * resample.middle95
 
+# shape is once again normal
+# range of middle 95 is ~6.02%, moe is ~ 3%
+# the margin of error by gallup is actually overestimated (4% compared to 3%)
+
+################################################################################
+# Simulation over n and p
+n <- 3000
+p <- 0.99
+num.polls <- 10000
+n.loop = seq(100, n, by = 10)
+p.loop <- seq(0.01, p, by = 0.01)
+
+np.sim.data <- tibble(n = numeric(), 
+                      p = numeric(), 
+                      moe.np = numeric())
+for (i in n.loop){
+  for(j in p.loop){
+    new.sim <- rbinom(n=num.polls, size = i, prob = j)/i
+    new.middle95 <- quantile(x=new.sim, 0.975) - quantile(x=new.sim, 0.025)
+    new.moe <- 0.5*new.middle95
+    np.sim.data <- bind_rows(np.sim.data, tibble(n=i, p=j, moe.np = new.moe))
+  }
+}
+ggplot(data = np.sim.data, aes(n,p, fill = moe.np))+
+  geom_raster()+
+  scale_fill_gradient()+
+  theme_bw()
